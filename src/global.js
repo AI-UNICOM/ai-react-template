87acd7c25286e56d278c364167bc4038cc71a170
weiqingting <weiqingting@hotmail.com>
2017-06-07 14:50:50 +0800

    修改bug

--- src/global.js
+++ src/global.js
@@ -1,8 +1,1 @@
-require('resources/base.css');
-require('resources/public.css');
-require('resources/componentBase.css');
-require('resources/common.scss');
-
-require("resources/assets/artDialog/css/ui-dialog.css");
-require("resources/assets/laydate/need/laydate.css");
-require("resources/assets/laydate/skins/default/laydate.css");
+console.log("Hello World!")
